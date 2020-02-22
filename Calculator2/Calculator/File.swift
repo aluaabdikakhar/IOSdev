bf26c639150acadc03f8e2d44ea9aadbe2578373
aluaabdikakhar <43954202+aluaabdikakhar@users.noreply.github.com>
2020-02-22 15:26:53 +0600

    lab2(2)

---
+++ Calculator2/Calculator/File.swift
@@ -0,0 +1,80 @@
+//
+//  File.swift
+//  Calculator
+//
+//  Created by apple on 2/22/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import Foundation
+
+class File {
+    // MARK: - Constants
+    enum Operation {
+        case allclear
+        case equals
+        case unary(function: (Double) -> Double)
+        case binary(function: (Double, Double) -> Double)
+    }
+    
+    var map: [String : Operation] = [
+        "+" : .binary { $0 + $1 },
+        "x" : .binary { $0 * $1 },
+        "-" : .binary { $0 - $1 },
+        "/" : .binary { $0 / $1 },
+        "=" : .equals,
+        "AC" : .allclear,
+        "%" : .unary { $0 / 100 },
+        "+/-" : .unary{ $0 * (-1) }
+    ]
+        
+    // MARK: - Variables
+    var result: Double = 0
+    var lastBinaryOperation: ((Double, Double) -> Double)?
+    var reminder: Double = 0
+    
+    // MARK: - Methods
+    func setOperand(number: Double) {
+        result = number
+    }
+    
+    func executeOperation(symbol: String) {
+        guard let operation = map[symbol] else {
+            print("ERROR: no such symbol in map")
+            return
+        }
+        
+        switch operation {
+        case .unary(let function):
+            if symbol == "%"{
+                if reminder == 0{
+                    result = function(result)
+                }else {
+                    result = reminder * function(result)
+                }
+            }else{
+                result = function(result)
+            }
+        case .binary(let function):
+            if lastBinaryOperation != nil{
+                executeOperation(symbol: "=")
+                reminder=result
+            }
+            else{
+                reminder=result
+            }
+            lastBinaryOperation = function
+        case .equals:
+            if let lastOperation = lastBinaryOperation {
+                result = lastOperation(reminder, result)
+                lastBinaryOperation = nil
+                reminder = 0
+            }
+        case .allclear:
+            result = 0
+            lastBinaryOperation = nil
+            reminder = 0
+        }
+    }
+}
+
---
+++ Calculator2/Calculator/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by apple on 2/18/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let model = File()
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        guard
+            let numberText = sender.titleLabel?.text
+        else { return }
+        if resultLabel.text == "0" || resultLabel.text == "0.0"{
+            resultLabel.text?.removeAll()
+            resultLabel.text?.append(String(numberText))
+        }else{
+            resultLabel.text?.append(String(numberText))
+        }
+    }
+    
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        guard
+            let numberText = resultLabel.text,
+            let number = Double(numberText),
+            let operation = sender.titleLabel?.text
+        else {
+            return
+        }
+        
+        resultLabel.text = "0"
+        model.setOperand(number: number)
+        model.executeOperation(symbol: operation)
+        if operation=="=" || operation=="AC" || operation=="%" || operation=="+/-"
+        {
+            resultLabel.text = "\(model.result)"
+        }
+    }
+}
+
